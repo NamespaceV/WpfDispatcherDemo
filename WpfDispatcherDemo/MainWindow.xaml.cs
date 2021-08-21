26c8e7c4865790f6853b1416a6acf95122648355
zephyr <s-zephyr@o2.pl>
2021-08-21 22:49:35 +0200

    update

--- WpfDispatcherDemo/MainWindow.xaml.cs
+++ WpfDispatcherDemo/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfDispatcherDemo
 {
@@ -28,27 +29,41 @@ namespace WpfDispatcherDemo
 
         public bool showDialog(string text)
         {
-            //if (!Dispatcher.CheckAccess())
-            //{
-            //    // We're not in the UI thread, ask the dispatcher to call this same method in the UI thread, then exit
-            //    Dispatcher.BeginInvoke(new Action(showDialog(text)));
-            //    return;
-            //}
+            if (!Dispatcher.CheckAccess())
+            {
+                // We're not in the UI thread, ask the dispatcher to call this same method in the UI thread
+                var onUiThread = Dispatcher.BeginInvoke(new Func<bool>(() => { return showDialog(text); }));
+                // wait for UI thread result
+                while (onUiThread.Status == DispatcherOperationStatus.Pending || onUiThread.Status == DispatcherOperationStatus.Executing)
+                {
+                    Thread.Sleep(100);//💩 active wait...
+                }
+                if (onUiThread.Status != DispatcherOperationStatus.Completed)
+                {
+                    throw new Exception("showDialog crashed");
+                }
+                // pass it back to non ui thread
+                return (bool)onUiThread.Result;
+            }
 
+            // were on UI Thread, show popup
             var r = MessageBox.Show(text, "Title", MessageBoxButton.YesNo);
-
             return r == MessageBoxResult.Yes;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Thread thread = new Thread(UpdateText);
+            // start not UI thread
+            Thread thread = new Thread(UpdateTextNonUiThread);
             thread.Start();
         }
 
-        private void UpdateText()
-        {
-            this.TextBox1.Text = showDialog("Bla bla bla") ? "YES ASDASD" : "NOOOOOOOOOOOOOOOOOO";
+        private void UpdateTextNonUiThread()
+        { 
+            // Try from non UI thread
+            var b = showDialog("Bla bla bla");
+            // dispatch TextBox update to UI thread
+            Dispatcher.Invoke(() => this.TextBox1.Text = b ? "YES ASDASD" : "NOOOOOOOOOOOOOOOOOO");
         }
     }
 }
